ba9e36ad0861003194e8dd0dffc8b81fb25e9121
LucaJ <Luca.Franken@student.hu.nl>
2022-12-14 14:41:25 +0100

    Kitchen done

--- src/main/java/KitchenSystem/MicroService/application/CommandHandler.java
+++ src/main/java/KitchenSystem/MicroService/application/CommandHandler.java
@@ -3,7 +3,11 @@ package KitchenSystem.MicroService.application;
 import KitchenSystem.MicroService.application.port.IngredientRepository;
 import KitchenSystem.MicroService.application.port.OrderRepository;
 import KitchenSystem.MicroService.application.port.ProductRepository;
+import KitchenSystem.MicroService.command.PlaceNewIngredient;
+import KitchenSystem.MicroService.command.PlaceNewProduct;
 import KitchenSystem.MicroService.command.PlaceOrder;
+import KitchenSystem.MicroService.domain.Amount;
+import KitchenSystem.MicroService.domain.Ingredient;
 import KitchenSystem.MicroService.domain.Order;
 import KitchenSystem.MicroService.domain.Product;
 import KitchenSystem.MicroService.infrastructure.driven.messaging.GenericEvent;
@@ -29,7 +33,7 @@ public class CommandHandler {
     private final WebClient webClient = WebClient.create();
 
 
-    public CommandHandler(OrderRepository orderRepository, ProductRepository productRepository, Producer producer) {
+    public CommandHandler(OrderRepository orderRepository, ProductRepository productRepository, IngredientRepository ingredientRepository, Producer producer) {
         this.orderRepository = orderRepository;
         this.productRepository = productRepository;
         this.ingredientRepository = ingredientRepository;
